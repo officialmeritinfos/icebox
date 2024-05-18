47b7f855c9baff97992085c05a6aa61f090a132e
Anon Dev <michaelerastus9@gmail.com>
2024-05-18 05:10:28 +0100

    Added livechat

--- resources/views/user/base.blade.php
+++ resources/views/user/base.blade.php
@@ -387,6 +387,17 @@ Footer start
 </script>
 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 <!-- Google language End -->
-
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+    var _smartsupp = _smartsupp || {};
+    _smartsupp.key = '5014cb7f85685d3fafa09e3361e9069ef651c73e';
+    window.smartsupp||(function(d) {
+        var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+        s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+        c.type='text/javascript';c.charset='utf-8';c.async=true;
+        c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+    })(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 </body>
 </html>
